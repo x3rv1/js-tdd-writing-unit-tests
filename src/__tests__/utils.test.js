c60c465c0bfc60e830a7d77a698f4ca207312676
Peter Xervi <xervipeter@gmail.com>
2025-10-18 01:34:07 +0300

    spell

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,8 @@
-// Your tests here
+import { pointsForWord } from "../utils";
+
+describe("pointsForWord", () => {
+  it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
+    const word = "test";
+    const points = pointsForWord(word);
+    expect(points).toBe(7);
+  });})
